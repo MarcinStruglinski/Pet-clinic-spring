1bb2e85586b5308f410bd02ffaa4b950f6ee2f34
Piotr Siwinski <piotr.siwinski@f-secure.com>
2018-06-24 10:07:15 +0200

    Introduced owner and pet entities

---
+++ src/main/java/pl/sda/poznan/spring/petclinic/model/Address.java
@@ -0,0 +1,17 @@
+package pl.sda.poznan.spring.petclinic.model;
+
+import javax.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class Address {
+
+  private String street;
+  private String postalcode;
+  private String city;
+  private String country;
+
+}
---
+++ src/main/java/pl/sda/poznan/spring/petclinic/model/Pet.java
@@ -0,0 +1,61 @@
+package pl.sda.poznan.spring.petclinic.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Pet {
+
+  private Long id;
+  private String name;
+  private Date birthDate;
+  private PetType petType;
+  private Owner owner;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public PetType getPetType() {
+    return petType;
+  }
+
+  public void setPetType(PetType petType) {
+    this.petType = petType;
+  }
+
+  @ManyToOne
+  public Owner getOwner() {
+    return owner;
+  }
+
+  public void setOwner(Owner owner) {
+    this.owner = owner;
+  }
+}

23f50d94a2ed9fcd9f8da7fefa45bf632b234885
Piotr Siwiński <piotr.siwinski@f-secure.com>
2018-07-21 17:07:56 +0200

    cleanup cors config

--- src/main/java/pl/sda/poznan/spring/petclinic/configuration/SecurityConfig.java
+++ src/main/java/pl/sda/poznan/spring/petclinic/configuration/SecurityConfig.java
@@ -9,56 +9,71 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import pl.sda.poznan.spring.petclinic.service.ApplicationUserDetailsService;
 
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final ApplicationUserDetailsService applicationUserDetailsService;
-    private final RestAuthenticationEntryPoint entryPoint;
-    private final RestLoginSuccessHandler successHandler;
-    private final RestLoginFailureHandler failureHandler;
-    private final CorsFilter corsFilter;
+  private final ApplicationUserDetailsService applicationUserDetailsService;
+  private final RestAuthenticationEntryPoint entryPoint;
+  private final RestLoginSuccessHandler successHandler;
+  private final RestLoginFailureHandler failureHandler;
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(applicationUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        .userDetailsService(applicationUserDetailsService)
+        .passwordEncoder(passwordEncoder());
+  }
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/api/v1/register").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(entryPoint)
-                .and()
-                .formLogin()
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .loginProcessingUrl("/api/v1/authenticate")
-                .successHandler(successHandler)
-                .failureHandler(failureHandler)
-                .and()
-                .logout()
-                .logoutUrl("/api/v1/logout")
-                .logoutSuccessHandler((request, response, authentication) -> response.setStatus(200))
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .csrf().disable().addFilterBefore(corsFilter, CsrfFilter.class);
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/h2-console/**").permitAll()
+        .antMatchers("/api/v1/register").permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(entryPoint)
+        .and()
+        .formLogin()
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .loginProcessingUrl("/api/v1/authenticate")
+        .successHandler(successHandler)
+        .failureHandler(failureHandler)
+        .and()
+        .logout()
+        .logoutUrl("/api/v1/logout")
+        .logoutSuccessHandler((request, response, authentication) -> response.setStatus(200))
+        .and()
+        .headers().frameOptions().disable()
+        .and()
+        .csrf().disable().addFilterBefore(corsFilter(), CsrfFilter.class);
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedOrigin("http://localhost:4200");
+    config.addAllowedMethod(CorsConfiguration.ALL);
+    config.addAllowedHeader(CorsConfiguration.ALL);
+    config.setAllowCredentials(true);
+    source.registerCorsConfiguration("/api/**", config);
+
+    return new CorsFilter(source);
+  }
 }

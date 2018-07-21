cad65672d4fe75b137f69782b72dca1a3d43942e
Michal Madajewski <mw.madajewski@gmail.com>
2018-07-21 14:23:48 +0200

    add registration validation

--- src/main/java/pl/sda/poznan/spring/petclinic/aop/ApplicationErrorHandler.java
+++ src/main/java/pl/sda/poznan/spring/petclinic/aop/ApplicationErrorHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import pl.sda.poznan.spring.petclinic.exception.ApplicationUserNotFoundException;
+import pl.sda.poznan.spring.petclinic.exception.EmailAlreadyRegisteredException;
 import pl.sda.poznan.spring.petclinic.exception.OwnerNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,11 @@ public class ApplicationErrorHandler {
     @ExceptionHandler(OwnerNotFoundException.class)
     public ResponseEntity handleOwnerNotFoundException() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(EmailAlreadyRegisteredException.class)
+    public ResponseEntity handleEmailAlreadyRegisteredException() {
+        return ResponseEntity.badRequest().build();
     }
 
     @ExceptionHandler(ApplicationUserNotFoundException.class)

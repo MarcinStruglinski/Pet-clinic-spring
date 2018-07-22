9940127180bf830a744b8f948e2a75439148fec8
Piotr Siwiński <piotr.siwinski@f-secure.com>
2018-07-22 11:35:53 +0200

    Updated main page

--- src/main/java/pl/sda/poznan/spring/petclinic/Application.java
+++ src/main/java/pl/sda/poznan/spring/petclinic/Application.java
@@ -1,12 +1,33 @@
 package pl.sda.poznan.spring.petclinic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    Environment env = SpringApplication.run(Application.class, args).getEnvironment();
+    String protocol = "http";
+    if (env.getProperty("server.ssl.key-store") != null) {
+      protocol = "https";
+    }
+    String hostAddress = "localhost";
+    log.info(
+        "\n----------------------------------------------------------\n\t"
+            + "Application '{}' is running! Access URLs:\n\t"
+            + "Local: \t\t{}://localhost:{}\n\t"
+            + "External: \t{}://{}:{}\n\t"
+            + "Profile(s): \t{}\n----------------------------------------------------------",
+        env.getProperty("spring.application.name"),
+        protocol,
+        env.getProperty("server.port"),
+        protocol,
+        hostAddress,
+        env.getProperty("server.port"),
+        env.getActiveProfiles());
   }
 }
